b2f15f0d6ad2ad272c6ec68c2530882b09107fea
Mazo667 <maxifava667@gmail.com>
2024-08-24 18:56:40 -0300

    Implementacion de la vista del mensaje

--- TableroMensajes/mensajes/urls.py
+++ TableroMensajes/mensajes/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('',TemplateView.as_view(template_name='mensajes/home.html'),name='home'),
-    path('recibidos/', TemplateView.as_view(template_name='mensajes/recibidos.html'),name='recibidos')
+    path('',
+         TemplateView.as_view(template_name='mensajes/home.html'),
+         name='home'
+         ),
+    path('recibidos/',
+          TemplateView.as_view(template_name='mensajes/recibidos.html'),
+          name='recibidos'
+          )
 ]
--- TableroMensajes/mensajes/views.py
+++ TableroMensajes/mensajes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Mensaje
 
 # Create your views here.
+def ver_mensajes(request):
+    mensajes_recibidos = Mensaje.objects
+
+    return render(request, 'mensajes/recibidos.html',{'mensajes' : mensajes_recibidos})
